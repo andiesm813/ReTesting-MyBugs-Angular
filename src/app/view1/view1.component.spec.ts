2e195ef9571c3ccf1160df7eba6e3838ddacc272
andiesm813 <86068582+andiesm813@users.noreply.github.com>
2025-01-08 11:38:31 -0300

    Re-testing My Bugs updates

--- src/app/view1/view1.component.spec.ts
+++ src/app/view1/view1.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
-import { IGX_DIALOG_DIRECTIVES, IgxButtonDirective, IgxRippleDirective } from '@infragistics/igniteui-angular';
+import { IgxButtonDirective, IgxRippleDirective, IgxToggleDirective, IgxToggleActionDirective, IgxOverlayOutletDirective, IGX_DATE_PICKER_DIRECTIVES, IGX_INPUT_GROUP_DIRECTIVES, IGX_DIALOG_DIRECTIVES } from '@infragistics/igniteui-angular';
 import { View1Component } from './view1.component';
 
 describe('View1Component', () => {
@@ -11,7 +11,7 @@ describe('View1Component', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ View1Component, NoopAnimationsModule, FormsModule, IGX_DIALOG_DIRECTIVES, IgxButtonDirective, IgxRippleDirective ]
+      imports: [ View1Component, NoopAnimationsModule, FormsModule, IgxButtonDirective, IgxRippleDirective, IgxToggleDirective, IgxToggleActionDirective, IgxOverlayOutletDirective, IGX_DATE_PICKER_DIRECTIVES, IGX_INPUT_GROUP_DIRECTIVES, IGX_DIALOG_DIRECTIVES ]
     })
     .compileComponents();
   });
