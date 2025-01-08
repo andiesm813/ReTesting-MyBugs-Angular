495bb0045383b61b73b8001f34806455a9867e75
andiesm813 <86068582+andiesm813@users.noreply.github.com>
2025-01-08 13:47:55 -0300

    Re-testing My Bugs updates

--- src/app/view1/view1.component.ts
+++ src/app/view1/view1.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
-import { IGX_DATE_PICKER_DIRECTIVES, IGX_DIALOG_DIRECTIVES, IGX_INPUT_GROUP_DIRECTIVES, IgxButtonDirective, IgxIconButtonDirective, IgxIconComponent, IgxOverlayOutletDirective, IgxRippleDirective, IgxToggleActionDirective, IgxToggleDirective } from 'igniteui-angular';
+import { FormsModule } from '@angular/forms';
+import { IGX_BANNER_DIRECTIVES, IGX_BUTTON_GROUP_DIRECTIVES, IGX_CARD_DIRECTIVES, IGX_CHIPS_DIRECTIVES, IGX_DATE_PICKER_DIRECTIVES, IGX_DIALOG_DIRECTIVES, IGX_INPUT_GROUP_DIRECTIVES, IGX_SELECT_DIRECTIVES, IGX_STEPPER_DIRECTIVES, IGX_TREE_DIRECTIVES, IgxButtonDirective, IgxIconButtonDirective, IgxIconComponent, IgxOverlayOutletDirective, IgxRippleDirective, IgxSnackbarComponent, IgxSwitchComponent, IgxToggleActionDirective, IgxToggleDirective } from 'igniteui-angular';
 
 @Component({
   selector: 'app-view1',
-  imports: [IGX_DATE_PICKER_DIRECTIVES, IGX_DIALOG_DIRECTIVES, IGX_INPUT_GROUP_DIRECTIVES, IgxButtonDirective, IgxIconButtonDirective, IgxIconComponent, IgxOverlayOutletDirective, IgxRippleDirective, IgxToggleActionDirective, IgxToggleDirective],
+  imports: [FormsModule, IGX_BANNER_DIRECTIVES, IGX_BUTTON_GROUP_DIRECTIVES, IGX_CARD_DIRECTIVES, IGX_CHIPS_DIRECTIVES, IGX_DATE_PICKER_DIRECTIVES, IGX_DIALOG_DIRECTIVES, IGX_INPUT_GROUP_DIRECTIVES, IGX_SELECT_DIRECTIVES, IGX_STEPPER_DIRECTIVES, IGX_TREE_DIRECTIVES, IgxButtonDirective, IgxIconButtonDirective, IgxIconComponent, IgxOverlayOutletDirective, IgxRippleDirective, IgxSnackbarComponent, IgxSwitchComponent, IgxToggleActionDirective, IgxToggleDirective],
   templateUrl: './view1.component.html',
   styleUrls: ['./view1.component.scss']
 })
 export class View1Component {
+  public checked: boolean = true;
 }
